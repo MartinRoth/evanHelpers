411d15ea88997ddb96c33f36f19fecdc01f2e1d5
MartinRoth <marsianus@gmail.com>
2015-11-18 15:03:55 +0100

    Initial commit

---
+++ R/manipulateCovariates.R
@@ -0,0 +1,20 @@
+#' Normalizes covariates
+#' @param covariate Covariate to be normalized
+#' @value list with three entries, \code{val} the normalized covariate,
+#'  \code{meanCov} the mean of the covariate, and \code{sdCov} its standard
+#'  deviation
+SetNormCovariate <- function(covariate) {
+  meanCov <- mean(covariate)
+  sdCov   <- sd(covariate)
+  val     <- (covariate - meanCov) / sdCov
+  return(list(meanCov = meanCov, sdCov = sdCov, val = val ))
+}
+
+#' Obtain new normalized covariates
+#' @description Returns the normalized value of a new covariate value with respect to an
+#' existing normCov object
+#' @param covariate The new value of the covariate
+#' @param normCov The normalized covariate object
+GetNormCovariate <- function(covariate, normCov) {
+  return((covariate - normCov$meanCov)/normCov$sdCov)
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+## evanHelpers
+
+Helper function for Extreme Value Analysis of Natural Hazards
