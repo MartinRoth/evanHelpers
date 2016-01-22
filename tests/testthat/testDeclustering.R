b281ceb6525b1bc0d8cd655649c9580a08654b0f
MartinRoth <marsianus@gmail.com>
2016-01-22 20:52:24 +0100

    Added BoxCox Adjustment function

--- tests/testthat/testDeclustering.R
+++ tests/testthat/testDeclustering.R
@@ -7,6 +7,8 @@ test_that("correct input handling", {
   expect_error(declusterSimpleSeparation(c(1,2,3), c(2,3,1), "foo"), "sep should be a numeric.")
   expect_error(declusterSimpleSeparation(c(1,2,3), c(2,3,1), c(1,2)), "sep should be a numeric.")
   expect_error(declusterSimpleSeparation(c(1,2,3), c(2,3), 1), "time and x must have same length.")
+  expect_error(GetBoxCoxAdjustment("a"), "data should be numeric")
+  expect_error(GetBoxCoxAdjustment(1), "data should have length > 1")
 })
 
 context("Output testing")
@@ -20,4 +22,6 @@ test_that("simple declustering output", {
   expect_equal(declusterSimpleSeparation(c(1,2,3,4), c(3,1,2,1), 2), 1)
   expect_equal(declusterSimpleSeparation(c(1,2,3,6,7,8,12,13),
                                          c(1,3,1,1,1,3,2,3),     2), c(2, 6, 8))
+  expect_equal(GetBoxCoxAdjustment(c(1,2,3)), 0)
+  expect_equal(GetBoxCoxAdjustment(c(1,5,9)), 3)
 })
