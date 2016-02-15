3907b8994f42fbd7fc6d8915b1db42e127168fe0
MartinRoth <marsianus@gmail.com>
2016-02-15 16:48:01 +0100

    Added gamlss modeling

---
+++ R/gamlssQuantileTrends.R
@@ -0,0 +1,49 @@
+#' Computes change using gamlss
+#' @description Fits a gamlss model and returns trend for the 0.05, 0.5, and 0.95 quantile
+#' @param formula Formula object
+#' @param data Data (so far only data.table is checked)
+#' @param family String either "BCCG" or "SN2"
+#' @param ... Other arguments passed to gamlss
+#' @return data.table with columns for  family, predictors, tau, and value
+#' @export
+#' @import data.table
+#' @import gamlss
+GetGamlssTrend <- function(formula, data, family = "BCCG", ...) {
+  if (!family %in% c("BCCG", "SN2")) stop(paste(family, "not available"))
+  adjust <- NA
+  #responseCol <- formula[[2]]
+  #dat <- copy(data)
+  dat <- get_all_vars(formula, data)
+  if (family == "BCCG") {
+    #adjust <- evanHelpers::GetBoxCoxAdjustment(dat[, eval(responseCol)])
+    #dat[, eval(responseCol) := eval(responseCol) + adjust]
+    adjust <- GetBoxCoxAdjustment(dat[, 1])
+    dat[, 1] <- dat[, 1] + adjust #eval(responseCol) := eval(responseCol) + adjust]
+  }
+  capture.output(fit <- gamlss(formula=formula, data = dat, family = gamlss.family(family), ...))
+  params <- list(mu = fit$mu.fv, sigma = fit$sigma.fv, nu = fit$nu.fv)
+  values <- data.table(family = family,
+                       tau    = rep(c(0.05, 0.5, 0.95), each = nrow(dat)),
+                       fitted = c(do.call(paste0("q", family), c(p=0.05, params)),
+                                  do.call(paste0("q", family), c(p=0.5,  params)),
+                                  do.call(paste0("q", family), c(p=0.95, params))))
+  if (family == "BCCG") values[, fitted := fitted - adjust]
+  result = values[, cbind(data[], family, fitted), by = tau]
+  #result[, eval(responseCol) := NULL]
+  return(result)
+}
+
+#' Prepare for Box-Cox transformation
+#' @description Box-Cox transformation is only allowed for positive values. In
+#' the case that the minimal value min one standard deviation is negative we add
+#' this difference.
+#' @param data Numeric vector consisting of the data
+#' @export
+GetBoxCoxAdjustment <- function(data) {
+  if(!class(data)=="numeric") stop("data should be numeric")
+  if(length(data)<2) stop("data should have length > 1")
+  x <- min(data)
+  y <- sd(data)
+  if (x < y) return(y - x)
+  else return(0)
+}
--- tests/testthat/testDeclustering.R
+++ tests/testthat/testDeclustering.R
@@ -1,5 +1,5 @@
 library(evanHelpers)
-context("Input testing")
+context("Input testing for declustering")
 
 test_that("correct input handling", {
   expect_error(declusterSimpleSeparation("foo"), "time should be a numeric vector.")
@@ -7,11 +7,9 @@ test_that("correct input handling", {
   expect_error(declusterSimpleSeparation(c(1,2,3), c(2,3,1), "foo"), "sep should be a numeric.")
   expect_error(declusterSimpleSeparation(c(1,2,3), c(2,3,1), c(1,2)), "sep should be a numeric.")
   expect_error(declusterSimpleSeparation(c(1,2,3), c(2,3), 1), "time and x must have same length.")
-  expect_error(GetBoxCoxAdjustment("a"), "data should be numeric")
-  expect_error(GetBoxCoxAdjustment(1), "data should have length > 1")
 })
 
-context("Output testing")
+context("Output testing for declustering")
 test_that("simple declustering output", {
   expect_equal(declusterSimpleSeparation(c(1,2,3),   c(2,3,1),   1), 2)
   expect_equal(declusterSimpleSeparation(c(1,5,10),  c(2,3,1),   1), c(1,2,3))
@@ -22,6 +20,4 @@ test_that("simple declustering output", {
   expect_equal(declusterSimpleSeparation(c(1,2,3,4), c(3,1,2,1), 2), 1)
   expect_equal(declusterSimpleSeparation(c(1,2,3,6,7,8,12,13),
                                          c(1,3,1,1,1,3,2,3),     2), c(2, 6, 8))
-  expect_equal(GetBoxCoxAdjustment(c(1,2,3)), 0)
-  expect_equal(GetBoxCoxAdjustment(c(1,5,9)), 3)
 })
